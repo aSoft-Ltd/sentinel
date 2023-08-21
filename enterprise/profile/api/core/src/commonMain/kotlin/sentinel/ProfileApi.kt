90c098341c0ba552da34c2ef40dec625c93fca01
George Sechu <admin@mitikaz.com>
2023-08-21 16:50:00 +0300

    Before auto merging main

--- enterprise/profile/api/core/src/commonMain/kotlin/sentinel/ProfileApi.kt
+++ enterprise/profile/api/core/src/commonMain/kotlin/sentinel/ProfileApi.kt
@@ -1,7 +1,11 @@
+@file:JsExport
+@file:Suppress("NON_EXPORTABLE_TYPE")
+
 package sentinel
 
 import identifier.OrganisationProfileApi
 import identifier.PersonalProfileApi
+import kotlin.js.JsExport
 
 interface ProfileApi {
     val personal: PersonalProfileApi
