269be80cb5c3c7801c5464ea87b1ad36de90ae14
andylamax <andylamax@programmer.net>
2023-09-13 07:27:34 +0300

    Removed espsilon in favor of epsilon-api and epslion-client

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ fun includeSubs(base: String, path: String = base, vararg subs: String) {
 
 listOf(
     "cinematic", "keep", "lexi", "captain", "neat", "kash", "geo", "kase",
-    "kronecker", "symphony", "epsilon", "krono", "hormone", "identifier",
+    "kronecker", "symphony", "epsilon-api", "epsilon-client", "krono", "hormone", "identifier",
     "kommerce", "kollections", "koncurrent", "kommander", "cabinet-api", "cabinet-picortex", "pione", "snitch"
 ).forEach { includeBuild("../$it") }
 
